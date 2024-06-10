2fbcdda3c24d73d89b5cbf3a6d878191423a518b
ShadowUser17 <linuxforce17@gmail.com>
2024-06-10 21:54:28 +0300

    Bump helm_release/minio to v14.6.7

--- main.tf
+++ main.tf
@@ -199,7 +199,7 @@ resource "helm_release" "minio" {
     chart = "minio"
     values = ["${file("./values/minio.yml")}"]
     name = "s3"
-    version = "14.6.2"
+    version = "14.6.7"
     namespace = "${kubernetes_namespace.testing-ns.id}"
     create_namespace = false
     depends_on = [helm_release.kube-prometheus-stack]
