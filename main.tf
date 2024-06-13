aaa757e9977722118e136051eb8fcd1319214975
ShadowUser17 <linuxforce17@gmail.com>
2024-06-13 17:11:10 +0300

    Bump helm_release/tetragon to v1.1.2

--- main.tf
+++ main.tf
@@ -217,7 +217,7 @@ resource "helm_release" "tetragon" {
     chart = "tetragon"
     values = ["${file("./values/tetragon.yml")}"]
     name = "tetragon"
-    version = "1.1.0"
+    version = "1.1.2"
     namespace = "kube-system"
     create_namespace = false
     depends_on = [helm_release.kube-prometheus-stack]
