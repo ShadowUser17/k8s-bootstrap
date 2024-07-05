9d384bffa796b2ee86f11a783f17e67eeca3adb6
ShadowUser17 <linuxforce17@gmail.com>
2024-07-05 16:40:00 +0300

    Bump helm_release/trivy-operator to v0.24.0

--- main.tf
+++ main.tf
@@ -292,7 +292,7 @@ resource "helm_release" "trivy-operator" {
     chart = "trivy-operator"
     values = ["${file("./values/trivy-operator.yml")}"]
     name = "trivy-operator"
-    version = "0.23.3"
+    version = "0.24.0"
     namespace = "trivy-system"
     create_namespace = true
     depends_on = [helm_release.kube-prometheus-stack]
