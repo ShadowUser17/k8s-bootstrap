3e098ddf3a0c5fbd18369c223e4350d32d612ff4
ShadowUser17 <linuxforce17@gmail.com>
2024-08-11 12:07:08 +0300

    Bump helm_release/argo-workflows to v0.41.14

--- main.tf
+++ main.tf
@@ -254,7 +254,7 @@ resource "helm_release" "argo-workflows" {
     chart = "argo-workflows"
     values = ["${file("./values/argo-workflows.yml")}"]
     name = "argo-workflows"
-    version = "0.41.13"
+    version = "0.41.14"
     namespace = "${kubernetes_namespace.argo-workflows-ns.id}"
     create_namespace = false
     depends_on = [
