d5b48fa24e8290d961985eafcf046aa629bb989e
ShadowUser17 <linuxforce17@gmail.com>
2024-09-13 20:45:52 +0300

    Bump helm_release/kube-prometheus-stack to v62.7.0

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "helm_release" "kube-prometheus-stack" {
     chart = "kube-prometheus-stack"
     values = ["${file("./values/prometheus-operator.yml")}"]
     name = "prom-operator"
-    version = "62.6.0"
+    version = "62.7.0"
     namespace = "${kubernetes_namespace.monitoring-stack-ns.id}"
     create_namespace = false
     depends_on = [helm_release.cert-manager, helm_release.nginx-ingress]
